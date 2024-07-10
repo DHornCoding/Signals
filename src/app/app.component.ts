f1489b0b6526eede5f460aeb1b4814efb972b369
DHO <david.horn@epg.com>
2024-07-10 08:35:35 +0200

    Exersice 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Signals';
+  title = signal('Hello, Angular Signal!');
+
+  updateTitle(){
+
+    if(this.title() === 'Hello, Angular Signal!'){
+    this.title.set('update');
+    }else{
+      this.title.set('Hello, Angular Signal!');
+    }
+  } 
 }
